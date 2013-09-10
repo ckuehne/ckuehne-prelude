a6336c684b8872203147a6527d2a8b8c3d5215b5
Conny Kuehne <conny.kuehne@gmail.com>
2013-09-10 11:51:19 +0200

    Added ckuehne-auctex.el and ckuehne-orgmode.el as real copies.

--- personal/ckuehne-auctex.el
+++ personal/ckuehne-auctex.el
@@ -1,1 +1,59 @@
-../../prelude/personal/ckuehne-auctex.el
+;; to install auctex for an existing brew emacs installation:
+;;   git clone git://git.sv.gnu.org/auctex.git; cd auctex
+;;   ./autogen.sh
+;;   ./configure --with-emacs=usr/local/bin/emacs --with-texmf-dir=/usr/local/share/texmf/
+;;      --with-lispdir=/usr/local/share/emacs/site-lisp
+;;   make
+;;   make install
+
+
+
+;; load
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+;; set default latex to pdflatex
+(setq latex-run-command "pdflatex")
+(setq TeX-PDF-mode t)
+
+;; un-fontify
+;; Only change sectioning colour
+(setq font-latex-fontify-sectioning 'color)
+;; super-/sub-script on baseline
+(setq font-latex-script-display (quote (nil)))
+;; Do not change super-/sub-script font
+(custom-set-faces
+ '(font-latex-subscript-face ((t nil)))
+ '(font-latex-superscript-face ((t nil)))
+ )
+;; Exclude bold/italic from keywords
+(setq font-latex-deactivated-keyword-classes
+      '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
+
+;; The following works on Mac OS X only.
+;; PDF syncing. Don't forget to activate syncing in Skim.
+;; source: http://tex.stackexchange.com/q/11613
+(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+
+(setq TeX-source-correlate-method 'synctex)
+
+(add-hook 'LaTeX-mode-hook
+          (lambda()
+            (add-to-list 'TeX-expand-list
+                         '("%q" skim-make-url))))
+
+(defun skim-make-url () (concat
+                         (TeX-current-line)
+                         " "
+                         (expand-file-name (funcall file (TeX-output-extension) t)
+                                           (file-name-directory (TeX-master-file)))
+                         " "
+                         (buffer-file-name)))
+(setq TeX-view-program-list
+      '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %q")))
+
+(setq TeX-view-program-selection '((output-pdf "Skim")))
+
+
+(provide 'ckuehne-auctex)
+;;; ckuehne-auctex.el ends here
--- personal/ckuehne-orgmode.el
+++ personal/ckuehne-orgmode.el
@@ -1,1 +1,37 @@
-../../prelude/personal/ckuehne-orgmode.el
+;; ckuehne: install latest org
+;; cd src
+;; git clone git://orgmode.org/org-mode.git
+;; cd org
+;; git checkout maint # change to maintenance branch
+;; make config
+;; change local.mk
+;;   SUDO= # empty, in order to not install as root into /usr/local
+;;   prefix	= /usr/local/share
+
+;; make up2 # this is the 'update git, install all' goal
+;; rm /usr/local/share/info/emacs/org.info.gz (this gets only installed if
+;; HOMEBREW_KEEP_INFO variable is set (HOMEBREW_KEEP_INFO should be set)
+
+
+;; capture templates
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Documents/gtd/todo.org" "Captured")
+         "* TODO %?\n %i")
+        ("j" "Journal" entry (file+datetree "~/Documents/gtd/org/journal.org")
+         "* %?\nEntered on %U\n %i")
+        ("m" "Memo to myself" entry (file "~/Documents/gtd/org/memos_to_myself.org")
+         "* %?\n %i")
+        ("d" "Ideas/Snippets/Thoughts dissertation" entry (file "~/Documents/gtd/org/ideas_dis\
+sertation.org")
+         "* %?\n %i")
+        ("p" "notes peersim chapter" entry (file+headline "~/Documents/_uni/diss/draft/dissnote\
+s.org" "Peersim")
+         "* %?\n %i")
+        ("b" "Useful Bash commands" entry (file "~/Documents/gtd/org/useful_bash_cmds.org")
+         "* %?\n %i")
+        ("r" "Recipes" entry (file "~/Documents/gtd/recipes.org")
+         "* %?\n %i")
+        ))
+
+(provide 'ckuehne-orgmode)
+;;; ckuehne-orgmode.el ends here

fa76a80ee497581df4070a32221b19540b635890
Conny Kuehne <conny.kuehne@gmail.com>
2013-10-02 14:29:16 +0200

    Configured org-capture mode.

--- personal/ckuehne-orgmode.el
+++ personal/ckuehne-orgmode.el
@@ -12,6 +12,11 @@
 ;; rm /usr/local/share/info/emacs/org.info.gz (this gets only installed if
 ;; HOMEBREW_KEEP_INFO variable is set (HOMEBREW_KEEP_INFO should be set)
 
+;; capture notess
+(global-set-key "\C-cc" 'org-capture)
+
+;; capture mode
+;; (setq org-default-notes-file (concat org-directory "/capture.org"))
 
 ;; capture templates
 (setq org-capture-templates
@@ -19,13 +24,14 @@
          "* TODO %?\n %i")
         ("j" "Journal" entry (file+datetree "~/Documents/gtd/org/journal.org")
          "* %?\nEntered on %U\n %i")
+        ("d" "d for diss")
+        ("d" "Ideas/Snippets/Thoughts dissertation" entry (file "~/Documents/gtd/org/ideas_dissertation.org")
+         "* %?\n %i")
+        ("dj" "Diss-Journal" entry (file+datetree "~/Projects/uni/diss/org/dissjournal.org")
+         "* %?\nEntered on %U\n %i")
         ("m" "Memo to myself" entry (file "~/Documents/gtd/org/memos_to_myself.org")
          "* %?\n %i")
-        ("d" "Ideas/Snippets/Thoughts dissertation" entry (file "~/Documents/gtd/org/ideas_dis\
-sertation.org")
-         "* %?\n %i")
-        ("p" "notes peersim chapter" entry (file+headline "~/Documents/_uni/diss/draft/dissnote\
-s.org" "Peersim")
+        ("p" "notes peersim chapter" entry (file+headline "~/Documents/_uni/diss/draft/dissnotes.org" "Peersim")
          "* %?\n %i")
         ("b" "Useful Bash commands" entry (file "~/Documents/gtd/org/useful_bash_cmds.org")
          "* %?\n %i")

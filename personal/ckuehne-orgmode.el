0b0fca501f9eac68d26d7d33fac525868489924c
Conny Kuehne <conny.kuehne@gmail.com>
2014-03-08 15:34:26 +0100

    some hooks for ediff orgmode zusammenspiel.

--- personal/ckuehne-orgmode.el
+++ personal/ckuehne-orgmode.el
@@ -16,6 +16,51 @@
 (global-set-key "\C-cc" 'org-capture)
 
 
+;; (add-hook 'ediff-prepare-buffer-hook 'f-ediff-prepare-buffer-hook-setup)
+;; (defun f-ediff-prepare-buffer-hook-setup ()
+;;   ;; specific modes
+;;   (cond ((eq major-mode 'org-mode)
+;;          (f-org-vis-mod-maximum))
+;;         ;; room for more modes
+;;         )
+;;   ;; all modes
+;;   (setq truncate-lines nil))
+;; (defun f-org-vis-mod-maximum ()
+;;   "Visibility: Show the most possible."
+;;   (cond
+;;    ((eq major-mode 'org-mode)
+;;     (visible-mode 1)  ; default 0
+;;     (setq truncate-lines nil)  ; no `org-startup-truncated' in hook
+;;     (setq org-hide-leading-stars t))  ; default nil
+;;    (t
+;;     (message "ERR: not in Org mode")
+;;     (ding))))
 
+;; diff hooks for org mode
+;; http://www.mail-archive.com/emacs-orgmode@gnu.org/msg74623.html
+(add-hook 'ediff-select-hook 'f-ediff-org-unfold-tree-element)
+(add-hook 'ediff-unselect-hook 'f-ediff-org-fold-tree)
+;; Check for org mode and existence of buffer
+(defun f-ediff-org-showhide(buf command &rest cmdargs)
+  "If buffer exists and is orgmode then execute command"
+  (if buf
+      (if (eq (buffer-local-value 'major-mode (get-buffer buf)) 'org-mode)
+          (save-excursion (set-buffer buf) (apply command cmdargs)))
+    )
+  )
+
+(defun f-ediff-org-unfold-tree-element ()
+  "Unfold tree at diff location"
+  (f-ediff-org-showhide ediff-buffer-A 'org-reveal)  
+  (f-ediff-org-showhide ediff-buffer-B 'org-reveal)  
+  (f-ediff-org-showhide ediff-buffer-C 'org-reveal)  
+  )
+;;
+(defun f-ediff-org-fold-tree ()
+  "Fold tree back to top level"
+  (f-ediff-org-showhide ediff-buffer-A 'hide-sublevels 1)  
+  (f-ediff-org-showhide ediff-buffer-B 'hide-sublevels 1)  
+  (f-ediff-org-showhide ediff-buffer-C 'hide-sublevels 1)  
+  )
 (provide 'ckuehne-orgmode)
 ;;; ckuehne-orgmode.el ends here

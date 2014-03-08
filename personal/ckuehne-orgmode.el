d92054f728a09554371fb466e7223cd6506c6069
Conny Kuehne <conny.kuehne@gmail.com>
2014-03-08 15:33:45 +0100

    put org capture config into own file.

--- personal/ckuehne-orgmode.el
+++ personal/ckuehne-orgmode.el
@@ -15,31 +15,7 @@
 ;; capture notess
 (global-set-key "\C-cc" 'org-capture)
 
-;; capture mode
-;; (setq org-default-notes-file (concat org-directory "/capture.org"))
 
-;; capture templates
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/Documents/gtd/todo.org" "Captured")
-         "* TODO %?\n %i")
-        ("f" "Food-Journal" entry (file+datetree "~/Documents/gtd/org/foodjournal.org")
-         "* %?\nEntered on %U\n %i")
-        ("j" "Journal" entry (file+datetree "~/Documents/gtd/org/journal.org")
-         "* %?\nEntered on %U\n %i")
-        ("d" "d for diss")
-        ("d" "Ideas/Snippets/Thoughts dissertation" entry (file "~/Documents/gtd/org/ideas_dissertation.org")
-         "* %?\n %i")
-        ("dj" "Diss-Journal" entry (file+datetree "~/Projects/uni/diss/org/dissjournal.org")
-         "* %?\nEntered on %U\n %i")
-        ("m" "Memo to myself" entry (file "~/Documents/gtd/org/memos_to_myself.org")
-         "* %?\n %i")
-        ("p" "notes peersim chapter" entry (file+headline "~/Documents/_uni/diss/draft/dissnotes.org" "Peersim")
-         "* %?\n %i")
-        ("b" "Useful Bash commands" entry (file "~/Documents/gtd/org/useful_bash_cmds.org")
-         "* %?\n %i")
-        ("r" "Recipes" entry (file "~/Documents/gtd/recipes.org")
-         "* %?\n %i")
-        ))
 
 (provide 'ckuehne-orgmode)
 ;;; ckuehne-orgmode.el ends here

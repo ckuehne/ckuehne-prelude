2d8ccaf45eeeb60281de31a9cabf48eb9ad9d68b
Conny Kuehne <conny.kuehne@gmail.com>
2015-07-05 12:03:29 +0200

    added vendor dir

--- init.el
+++ init.el
@@ -8,13 +8,26 @@
   "Pieces of the core functionality of prelude.")
 (defvar prelude-savefile-dir (expand-file-name "savefile" dot-emacs-dot-d)
   "This folder stores all the automatically generated save/history-files.")
+(defvar prelude-vendor-dir (expand-file-name "vendor" dot-emacs-dot-d)
+  "This directory houses packages that are not yet available in ELPA (or MELPA).")
 
 (unless (file-exists-p prelude-savefile-dir)
   (make-directory prelude-savefile-dir))
 
+(defun prelude-add-subfolders-to-load-path (parent-dir)
+  "Add all first level PARENT-DIR subdirs to the `load-path'."
+  (dolist (f (directory-files parent-dir))
+    (let ((name (expand-file-name f parent-dir)))
+      (when (and (file-directory-p name)
+                 (not (equal f ".."))
+                 (not (equal f ".")))
+        (add-to-list 'load-path name)))))
+
 ;; add config directories to Emacs's `load-path'
 (add-to-list 'load-path pieces-of-prelude-dir)
 (add-to-list 'load-path ckuehne-personal-dir)
+(add-to-list 'load-path prelude-vendor-dir)
+(prelude-add-subfolders-to-load-path prelude-vendor-dir)
 
 ;; functionality taken directly from prelude
 (require 'prelude-packages)
